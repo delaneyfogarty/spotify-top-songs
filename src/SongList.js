8950b1dc15ea2e4c504047a006b782f442522822
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-12 17:48:51 -0700

    song page

--- src/SongList.js
+++ src/SongList.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import Song from './Song.js';
+import './App.css';
 
-export default function SongList() {
-  return <div>SongList</div>;
+export default function SongList({ songs }) {
+  return (
+    <div className="songs">
+      {songs.map((song) => (
+        <Song key={song.id} song={song} />
+      ))}
+    </div>
+  );
 }
--- src/SongPage.js
+++ src/SongPage.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import './App.css';
+import { getSongs } from './services/fetch-utils';
+import SongList from './SongList';
 
-export default function SongPage() {
-  return <div>SongPage</div>;
+function App() {
+  const [songs, setSongs] = useState([]);
+  const [page, setPage] = useState(1);
+  const perPage = 40;
+
+  useEffect(() => {
+    async function fetchSongs() {
+      const from = page * perPage - perPage;
+      const to = page * perPage;
+      const songsData = await getSongs(from, to);
+
+      setSongs(songsData);
+    }
+    fetchSongs();
+  }, [page]);
+
+  return (
+    <>
+      <h2>Current Page {page}</h2>
+      <div className="buttons">
+        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+          Previous Page
+        </button>
+        <button onClick={() => setPage(page + 1)}>Next Page</button>
+      </div>
+      <SongList songs={songs} />
+    </>
+  );
 }
+
+export default App;
