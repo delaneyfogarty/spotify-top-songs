9e7a34b7199b3a932bab2fd3cb7715c671205a20
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-13 10:18:12 -0700

    song.js complete

--- src/Song.js
+++ src/Song.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Song() {
-  return <div>Song</div>;
+export default function Song({ song }) {
+  return (
+    <div>
+      <Link to={`/songs/${song.id}`}>Detail Page</Link>
+      <h1>{song.track_name}</h1>
+      <p>{song.artist_name}</p>
+    </div>
+  );
 }
--- src/SongDetail.js
+++ src/SongDetail.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { getSingleSong } from './services/fetch-utils';
 
 export default function SongDetail() {
-  return <div>SongDetail</div>;
+  const [song, setSong] = useState({});
+  const params = useParams();
+
+  useEffect(() => {
+    async function fetchSingleSong() {
+      const songData = await getSingleSong(params.id);
+
+      setSong(songData);
+    }
+
+    fetchSingleSong();
+
+  }, [params.id]);
+
+
+  return (
+  <div className="song-detail">
+    <h3>{song.track_name}</h3>
+    <h4>{song.artist_name}</h4>
+    <p>Popularity Score: {song.popularity}</p>
+    <p>Danceability Score: {song.danceability}</p>
+    <p>Loudness (Higher the value, louder the song): {song.loudness}</p>
+    <p>How likely the song is a live recording: {song.liveness}</p>
+    <p>Duration in milliseconds: {song.duration_ms}</p>
+  </div>
 }
